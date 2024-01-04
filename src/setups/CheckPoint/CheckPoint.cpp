5fd4e184b57273f41a74c3500b93dc1812f8cc19
PhillipRothenbeck <philliprothenbeck@gmail.com>
2024-01-04 12:33:07 +0100

    fix whitespace problem

--- src/setups/CheckPoint/CheckPoint.cpp
+++ src/setups/CheckPoint/CheckPoint.cpp
@@ -14,28 +14,28 @@ tsunami_lab::setups::CheckPoint::CheckPoint(t_real i_dimX,
                                             t_real i_dimY,
                                             t_idx i_nx,
                                             t_idx i_ny,
-														  t_idx i_frame,
+                                            t_idx i_frame,
                                             t_real *i_height,
                                             t_real *i_momentumX,
                                             t_real *i_momentumY,
                                             t_real *i_bathymetry,
-														  t_real *i_time) {
+                                            t_real *i_time) {
     m_nx = i_nx;
     m_ny = i_ny;
-	 m_frameOffset = (i_frame - 1) * i_nx * i_ny;
+    m_frameOffset = (i_frame - 1) * i_nx * i_ny;
     m_height = i_height;
     m_momentumX = i_momentumX;
     m_momentumY = i_momentumY;
     m_bathymetry = i_bathymetry;
-	 m_time = i_time;
-	 m_currentFrame = i_frame;
+    m_time = i_time;
+    m_currentFrame = i_frame;
 
     m_cellWidthX = m_nx / i_dimX;
     m_cellWidthY = m_ny / i_dimY;
 
-	 m_heightData = new t_real[(m_nx + 2) * (m_ny + 2)];
-	 m_momentumXData = new t_real[(m_nx + 2) * (m_ny + 2)];
-	 m_momentumYData = new t_real[(m_nx + 2) * (m_ny + 2)];
+    m_heightData = new t_real[(m_nx + 2) * (m_ny + 2)];
+    m_momentumXData = new t_real[(m_nx + 2) * (m_ny + 2)];
+    m_momentumYData = new t_real[(m_nx + 2) * (m_ny + 2)];
 }
 
 tsunami_lab::t_real tsunami_lab::setups::CheckPoint::getHeight(t_real i_x,
@@ -71,13 +71,13 @@ tsunami_lab::t_real tsunami_lab::setups::CheckPoint::getBathymetry(t_real i_x,
 }
 
 tsunami_lab::setups::CheckPoint::~CheckPoint() {
-	delete[] m_heightData;
-	delete[] m_momentumXData;
-	delete[] m_momentumYData;
+    delete[] m_heightData;
+    delete[] m_momentumXData;
+    delete[] m_momentumYData;
 
-	delete[] m_height;
-	delete[] m_momentumX;
-	delete[] m_momentumY;
-	delete[] m_bathymetry;
-	delete[] m_time;
+    delete[] m_height;
+    delete[] m_momentumX;
+    delete[] m_momentumY;
+    delete[] m_bathymetry;
+    delete[] m_time;
 }
