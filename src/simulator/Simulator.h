ba15963d35e6f1884d7b53bfefd38581c7ff1968
129c80f8-f1c7-4e1f-8c67-43c93f8c7672 <fi65mip@login01.ara>
2024-01-15 15:11:38 +0100

    added structs and function

--- src/simulator/Simulator.h
+++ src/simulator/Simulator.h
@@ -19,10 +19,37 @@ namespace tsunami_lab {
 }
 
 class tsunami_lab::simulator {
+   private:
+   static void initParallelData (t_idx i_globalNX, t_idx i_globalNY, ParallelData *o_parallelData);
    public:
+   struct ParallelData {
+    t_idx size;
+    t_idx rank;
+    t_idx up, down, left, right;
+    MPI_Comm communicator;
+    MPI_Request requesst[8];
+    MPI_Datatype row;
+    MPI_Datatype column;
+    MPI_Datatype subarray;
+    MPI_Datatype file;
+    MPI_Datatype restart; 
+    }
+   struct Grid {
+    t_idx localNX;
+    t_idx localNY;
+    t_idx globalNX;
+    t_idx globalNY;
+    t_real dX;
+    t_real dY;
+    t_real *height;
+    t_real *momentumX;
+    t_real *momentumY;
+    t_real *bathymetry; 
+    }
     static void runSimulation(tsunami_lab::setups::Setup *i_setup,
                               tsunami_lab::t_real i_hStar,
                               tsunami_lab::configs::SimConfig i_simConfig);
 };
+
 
 #endif
