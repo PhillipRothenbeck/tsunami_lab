1be432fb61f7e221fa00a9a13613cd81e2e40613
0c527be4-6014-4d48-9ddb-7026cd1c38a8 <ri26lit@login01.ara>
2024-01-20 16:02:16 +0100

    Take MPI routines from Simulator

--- src/simulator/Simulator.h
+++ src/simulator/Simulator.h
@@ -23,36 +23,7 @@ namespace tsunami_lab {
     class simulator;
 }
 
-struct ParallelData {
-    tsunami_lab::t_idx size;
-    tsunami_lab::t_idx rank;
-    tsunami_lab::t_idx up, down, left, right;
-    MPI_Comm communicator;
-    MPI_Request requesst[8];
-    MPI_Datatype row;
-    MPI_Datatype column;
-    MPI_Datatype text;
-    MPI_Datatype file;
-    MPI_Datatype restart;
-};
-
-struct Grid {
-    tsunami_lab::t_idx localNX;
-    tsunami_lab::t_idx localNY;
-    tsunami_lab::t_idx globalNX;
-    tsunami_lab::t_idx globalNY;
-    tsunami_lab::t_real dX;
-    tsunami_lab::t_real dY;
-    tsunami_lab::t_real *height;
-    tsunami_lab::t_real *momentumX;
-    tsunami_lab::t_real *momentumY;
-    tsunami_lab::t_real *bathymetry;
-};
-
 class tsunami_lab::simulator {
-   private:
-    // static void initParallelData(t_idx i_globalNX, t_idx i_globalNY, ParallelData *o_parallelData);
-
    public:
     static void runSimulation(tsunami_lab::setups::Setup *i_setup,
                               tsunami_lab::configs::SimConfig i_simConfig);
