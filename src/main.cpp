276cae5231e43f8234f1a079303971233ac1063d
0c527be4-6014-4d48-9ddb-7026cd1c38a8 <ri26lit@login01.ara>
2024-01-22 12:12:57 +0100

    finish parallel data definition

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
  * Entry-point for simulations.
  **/
 #include <mpi.h>
-#include <cassert>
 
+#include <cassert>
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
@@ -24,18 +24,19 @@ int main(int i_argc, char *i_argv[]) {
     // MPI_Init and error handling
     int l_error = MPI_Init(&i_argc, &i_argv);
     assert(l_error == MPI_SUCCESS);
-    
+
     // get rank and communicator size and error handling
     int l_rank, l_worldSize;
     int l_nx, l_ny;
     l_error = MPI_Comm_size(MPI_COMM_WORLD, &l_worldSize);
     assert(l_error == MPI_SUCCESS);
-    l_error =MPI_Comm_rank(MPI_COMM_WORLD, &l_rank);
+    l_error = MPI_Comm_rank(MPI_COMM_WORLD, &l_rank);
     assert(l_error == MPI_SUCCESS);
-    
+
     // timer and setup
     Timer *l_timer = new Timer();
-    tsunami_lab::setups::Setup *l_setups = nullptr;
+    tsunami_lab::setups::Setup *l_setup = nullptr;
+    tsunami_lab::configs::SimConfig l_simConfig = tsunami_lab::configs::SimConfig();
 
     // only rank 0 executes this part
     if (l_rank == 0) {
@@ -66,26 +67,24 @@ int main(int i_argc, char *i_argv[]) {
             }
         }
 
-        tsunami_lab::configs::SimConfig l_simConfig = tsunami_lab::configs::SimConfig();
-
         if (l_flagConfig.useTiming()) l_timer->start();
         // load parameters from runtimeConfig.json
         tsunami_lab::t_idx err = tsunami_lab::io::ConfigLoader::loadConfig(l_configName,
                                                                            l_flagConfig,
-                                                                           l_setups,
+                                                                           l_setup,
                                                                            l_simConfig);
         if (l_flagConfig.useTiming()) l_timer->printTime("Loading Config ");
 
         if (err != 0) {
             std::cout << "failed to read: " << l_configName << std::endl;
-            delete l_setups;
+            delete l_setup;
             delete l_timer;
             return EXIT_FAILURE;
         }
+
+        l_nx = l_simConfig.getXCells();
+        l_ny = l_simConfig.getYCells();
         for (int i = 1; i < l_worldSize; i++) {
-            l_nx = l_simConfig.getXCells(); // muss das jedes mal neu gefetched werden?
-            l_ny = l_simConfig.getYCells(); // muss das jedes mal neu gefetched werden?
-            
             // Send x / y to every process != 0
             l_error = MPI_Send(&l_nx, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             assert(l_error == MPI_SUCCESS);
@@ -100,26 +99,23 @@ int main(int i_argc, char *i_argv[]) {
         assert(l_error == MPI_SUCCESS);
     }
 
-    std::cout << "Process with rank " << l_rank << " has nx: " << l_nx << " and ny: " << l_ny << std::endl;
-
     // create ParralelData struct
-    // tsunami_lab::MPIKernel::ParallelData *l_parallelData = new tsunami_lab::MPIKernel::ParallelData();
     tsunami_lab::MPIKernel::ParallelData l_parallelData;
-    
-    // init parallel data
-    tsunami_lab::MPIKernel::initParallelData(l_nx, l_ny, &l_parallelData);
-    std::cout << l_parallelData.rank << " has neighbors (up, down, left, right): " << l_parallelData.up << ", " << l_parallelData.down << ", " << l_parallelData.left << ", " << l_parallelData.right << std::endl;
-    // start simulation from config
-    // tsunami_lab::simulator::runSimulation(l_setups, l_simConfig);
+    tsunami_lab::MPIKernel::Grid l_grid;
 
-    delete l_setups;
+    // init parallel data
+    tsunami_lab::MPIKernel::init(l_nx, l_ny, &l_parallelData, &l_grid);
+
+    // start simulation from config
+    tsunami_lab::simulator::runSimulation(l_setup, l_simConfig, l_parallelData, l_grid);
+
+    delete l_setup;
     delete l_timer;
 
     // free parallel datatypes
     tsunami_lab::MPIKernel::freeParallelData(&l_parallelData);
-    // delete l_parallelData;
     std::cout << "finished, exiting" << std::endl;
-    
+
     l_error = MPI_Finalize();
     assert(l_error == MPI_SUCCESS);
     return EXIT_SUCCESS;
