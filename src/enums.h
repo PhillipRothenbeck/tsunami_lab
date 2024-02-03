dacf52900b478e106453c54484774903d479dab4
Marek Sommerfeld <marek1999@online.de>
2024-02-03 15:21:39 +0100

    new enum header

---
+++ src/enums.h
@@ -0,0 +1,15 @@
+/**
+ * @author Marek Sommerfeld (marek.sommerfeld AT uni-jena.de)
+ *
+ * @section DESCRIPTION
+ * Enum definitions used throughout the code.
+ **/
+#ifndef TSUNAMI_LAB_ENUMS_H
+#define TSUNAMI_LAB_ENUMS_H
+
+namespace tsunami_lab {
+    //! ghost cell sweep mode enum
+    enum e_sweepMode { HORIZONTAL, VERTICAL };
+}  // namespace tsunami_lab
+
+#endif
