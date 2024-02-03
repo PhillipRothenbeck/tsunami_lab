60ded861379becb50c25341da7e924a5370ba5b5
Marek Sommerfeld <marek1999@online.de>
2024-02-03 12:54:31 +0100

    timer output flag integration

--- src/timer.h
+++ src/timer.h
@@ -15,12 +15,13 @@ class Timer {
    private:
     typedef std::chrono::high_resolution_clock::time_point t_time;
     t_time m_startTime;
+    bool m_useTiming;
 
    public:
     /**
      * @brief timer constructer, runs timer->start() when init
      */
-    Timer() {
+    Timer(bool i_useTiming) {
         start();
     }
 
@@ -35,19 +36,25 @@ class Timer {
      * @brief resets timer to current time
      */
     void start() {
+        if (!m_useTiming)
+            return;
         m_startTime = now();
     }
 
     /**
      * @brief prints out time took since last reset, resets time afterwards
      */
-    void printTime(std::string i_description = "", int rank = -1) {
+    void printTime(std::string i_description = "", int i_rank = -1) {
+        if (!m_useTiming)
+            return;
+
         std::chrono::duration<double> l_chronoTimeElapsed = now() - m_startTime;
 
-        if (rank != -1) std::cout << "Rank " << rank << ": ";
+        if (i_rank != -1) std::cout << "Rank " << std::to_string(i_rank) << ": ";
         std::cout << "Took " << l_chronoTimeElapsed.count() << "s";
         if (i_description.compare("") != 0) std::cout << " for " << i_description;
         std::cout << std::endl;
+
         start();
     }
 };
