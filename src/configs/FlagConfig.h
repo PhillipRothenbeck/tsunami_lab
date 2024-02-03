708f4c1995d982f6d0b6d9d886f8ec206343cfb0
Marek Sommerfeld <marek1999@online.de>
2024-02-03 19:42:09 +0100

    code cleanup

--- src/configs/FlagConfig.h
+++ src/configs/FlagConfig.h
@@ -1,16 +1,11 @@
 /**
- * @author Bohdan Babii, Phillip Rothenbeck
+ * @author Marek Sommerfeld (marek.sommerfeld AT uni-jena.de)
  *
  * @section DESCRIPTION
  * Configuration that holds all information for flags.
  **/
 #ifndef TSUNAMI_LAB_FLAG_CONFIG_H
 #define TSUNAMI_LAB_FLAG_CONFIG_H
-
-#include <cstdlib>
-#include <string>
-
-#include "../constants.h"
 
 namespace tsunami_lab {
     namespace configs {
@@ -20,40 +15,24 @@ namespace tsunami_lab {
 
 class tsunami_lab::configs::FlagConfig {
    private:
-    bool m_useCheckPoint = false;
+    //! use timing flag
     bool m_useTiming = false;
+    //! use IO flag
     bool m_useIO = true;
 
    public:
     /**
-     * Constructs a flag configuration object.
+     * @brief Constructs a flag configuration object.
      *
      */
     FlagConfig() {
     }
 
     /**
-     * @brief Gets the useCheckPoint flag.
-     *
-     * @return wether checkpoint should be used.
-     */
-    bool useCheckPoint() {
-        return m_useCheckPoint;
-    }
-
-    /**
-     * @brief Set the useCheckPoint flag.
-     *
-     * @param i_value value of the useCheckPoint flag.
-     */
-    void setUseCheckPoint(bool i_value) {
-        m_useCheckPoint = i_value;
-    }
-
-    /**
      * @brief Gets the useTiming flag.
      *
-     * @return wether timing should be used.
+     * @return true: timing should be used.
+     * @return false: timing should not be used.
      */
     bool useTiming() {
         return m_useTiming;
@@ -68,10 +47,21 @@ class tsunami_lab::configs::FlagConfig {
         m_useTiming = i_value;
     }
 
+    /**
+     * @brief Gets the useIO flag.
+     *
+     * @return true: output file should be written.
+     * @return false: output file should not be written.
+     */
     bool useIO() {
         return m_useIO;
     }
 
+    /**
+     * @brief Set the useIO flag.
+     *
+     * @param i_value value of the useIO flag.
+     */
     void setUseIO(bool i_value) {
         m_useIO = i_value;
     }
